9899074eece8d94420363e182b48882a169161af
syimleang <syimleang@myseneca.ca>
2024-04-27 01:09:13 -0400

    Add error handling and improve post function

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ const apiUrl = "https://api.quotable.io/random";
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 
+// Get quote from "quotable" API
 async function getQuote(url) {
-  const response = await fetch(url);
-  var data = await response.json();
-  quote.textContent = data.content;
-  author.textContent = data.author;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const fullQuote = `${data.content} --- by ${data.author}`;
+        quote.textContent = data.content;
+        author.textContent = data.author;
+    } catch (error) {
+        console.log("Error fetching quote: ", error);
+    }
 }
 
 getQuote(apiUrl);
 
+// Post function on X
 const post = () =>
   window.open(
-    "https://twitter.com/intent/tweet?text=" +
-      quote.textContent +
-      "--- by " +
-      author.textContent,
+    `https://twitter.com/intent/tweet?text=${quote.textContent} --- by ${author.textContent}`,
     "post Window",
-    "width=600, height=300"
+    "width=800, height=500"
   );
